7a4b09bb507a7e8321d2fdadaa1c241151916e00
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-01-24 20:30:09 +0200

    Add files via upload

---
+++ JS Basic/A01FirstStepInCode/E02num1to10.js
@@ -0,0 +1,6 @@
+function e02num1to10() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i)
+    }
+}
+e02num1to10()
---
+++ JS Basic/A01FirstStepInCode/E03SquareArea.js
@@ -0,0 +1,7 @@
+function e03SquareArea(input) {
+    let a = Number(input[0]);
+    let area = a * a;
+
+    console.log(area);
+}
+e03SquareArea([4])
