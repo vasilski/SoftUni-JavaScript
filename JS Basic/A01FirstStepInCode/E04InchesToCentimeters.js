47b3bcbbe301d2b8658babe4cf6aacb8c707675c
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-01-25 07:11:51 +0200

    Add files via upload

---
+++ JS Basic/A01FirstStepInCode/E04InchesToCentimeters.js
@@ -0,0 +1,7 @@
+function e04InchesToCentimeters(input) {
+    let inch = Number(input);
+    let centimeters = inch * 2.54;
+
+    console.log(centimeters);
+}
+e04InchesToCentimeters(7)
---
+++ JS Basic/A01FirstStepInCode/E05GreetingByName.js
@@ -0,0 +1,5 @@
+function E05GreetingByName(input) {
+    let name = (input[0]);
+    console.log(`Hello, ${name}!`);
+}
+E05GreetingByName(["Vasko"])
