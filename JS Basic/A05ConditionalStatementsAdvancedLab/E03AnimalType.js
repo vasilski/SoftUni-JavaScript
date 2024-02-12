0145355c8e9f6cc8ece7e3e57a81950f8206238d
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-02-12 06:39:33 +0200

    Add files via upload

---
+++ JS Basic/A05ConditionalStatementsAdvancedLab/E03AnimalType.js
@@ -0,0 +1,21 @@
+function e03AnimalType(input) {
+    let animal = input[0];
+    switch (animal) {
+        case `dog`:
+            console.log("mammal");
+            break;
+        case `crocodile`:
+            console.log("reptile");
+            break;
+        case `tortoise`:
+            console.log("reptile");
+            break;
+        case `snake`:
+            console.log("reptile");
+            break;
+        default:
+            console.log("unknown");
+            break;
+    }
+}
+e03AnimalType("dog");
