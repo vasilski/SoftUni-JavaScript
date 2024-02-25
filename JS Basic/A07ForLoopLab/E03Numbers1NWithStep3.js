eaf6d1979b0a0d709f1f509f5dab77b2f1d2402d
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-02-25 23:25:59 +0200

    Add files via upload

---
+++ JS Basic/A07ForLoopLab/E03Numbers1NWithStep3.js
@@ -0,0 +1,7 @@
+function e03Numbers1NWithStep3(input) {
+    let number = Number(input[0]);
+    for (let i = 1; i <= number; i+= 3) {
+        console.log(i)
+    }
+}
+e03Numbers1NWithStep3(["15"])
